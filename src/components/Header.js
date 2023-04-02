7b71504bf0d34c6a648421ba297b2428ed2e9be4
yhyem <yoonhyemin0120@gmail.com>
2023-04-02 22:47:03 +0900

    design: Header bottom content 추가

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,8 +9,8 @@ import loginCustomer from '../assets/images/loginCustomer.png';
 const Header = () => {
   return (
     <>
-      <WrapHeaderTop>
-        <HeaderTop>
+      <WrapHeaderBlock>
+        <HeaderBlock>
           <LogoImg src={logoRed} />
           <Title>C U L T U R E P L E X</Title>
           <RightBlock>
@@ -31,20 +31,35 @@ const Header = () => {
               <HeaderContent>고객센터</HeaderContent>
             </WrapHeaderImg>
           </RightBlock>
-        </HeaderTop>
-      </WrapHeaderTop>
+        </HeaderBlock>
+      </WrapHeaderBlock>
       <LineBlock />
+      <WrapHeaderBottomBlock>
+        <HeaderBlock>
+          <BottomContent>영화</BottomContent>
+          <BottomContent>예매</BottomContent>
+          <BottomContent>스토어</BottomContent>
+          <BottomContent>이벤트</BottomContent>
+        </HeaderBlock>
+      </WrapHeaderBottomBlock>
+      <RedLineBlock />
     </>
   );
 };
 
-const WrapHeaderTop = styled.div`
+const WrapHeaderBlock = styled.div`
   display: flex;
   padding: 20px;
   justify-content: center;
 `;
 
-const HeaderTop = styled.div`
+const WrapHeaderBottomBlock = styled.div`
+  display: flex;
+  padding: 15px 20px;
+  justify-content: center;
+`;
+
+const HeaderBlock = styled.div`
   width: 1200px;
   display: flex;
 `;
@@ -82,6 +97,17 @@ const HeaderContent = styled.div`
 const LineBlock = styled.div`
   height: 1px;
   background-color: #eeeeee;
+`;
+
+const BottomContent = styled.div`
+  font-size: 17px;
+  font-weight: bold;
+  padding-right: 50px;
+`;
+
+const RedLineBlock = styled.div`
+  height: 2px;
+  background-color: #fa4357;
 `;
 
 export default Header;
