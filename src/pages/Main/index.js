d98d0337fd2732285bb6a89059598f9e61ad7311
yhyem <yoonhyemin0120@gmail.com>
2023-04-11 17:33:32 +0900

    design: 메인 페이지 이벤트 영역 스타일링

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -2,12 +2,14 @@ import { useState } from 'react';
 
 import Banner from './Banner';
 import Ranking from './Ranking';
+import EventList from './EventList';
 
 const Main = () => {
   return (
     <>
       <Banner />
       <Ranking />
+      <EventList />
     </>
   );
 };
